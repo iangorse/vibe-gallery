4ef2792e87fa28b06d00a14b0ac20de29be24434
Ian Gorse <ian.gorse@outlook.com>
2025-08-16 16:18:32 +0100

    Refactor image rendering in App component for improved layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-
-import './App.css';
-import imageList from './imageList.json';
-
-const images = imageList.map(name => `/images/${name}`);
-
-function App() {
   return (
     <div className="App container">
       <h1 className="my-4 text-center">Vibe Gallery</h1>
-      <div className="row g-3">
-        {images.map((src, idx) => (
-          <div className="col-6 col-sm-4 col-md-3 col-lg-2" key={idx}>
-            <div className="card h-100 shadow-sm">
-              <img src={src} alt={`Gallery ${idx + 1}`} className="card-img-top img-fluid" style={{objectFit: 'cover', maxHeight: '180px'}} />
+      <div className="row g-0">
+        {imageList.map((filename, idx) => (
+          <div className="col-6 col-md-4 col-lg-3 p-0" key={idx}>
+            <div className="card h-100 border-0 rounded-0">
+              <img
+                src={imageBasePath + filename}
+                className="card-img-top rounded-0"
+                alt={filename}
+                style={{ objectFit: "cover", height: "100%" }}
+              />
             </div>
           </div>
         ))}
       </div>
     </div>
   );
-}
-
-export default App;
