5023125d35d14d25db1629b34abe55e98c41e1cd
Ian Gorse <ian.gorse@outlook.com>
2025-08-16 16:21:07 +0100

    Implement fullscreen image viewing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import imageList from "./imageList.json";
+import { useState } from "react";
 
 const imageBasePath = "/images/";
 
 function App() {
+  const [fullscreenImg, setFullscreenImg] = useState(null);
+
+  const handleImgClick = (filename) => {
+    setFullscreenImg(filename);
+  };
+
+  const handleClose = () => {
+    setFullscreenImg(null);
+  };
+
   return (
-  <div className="App container-fluid">
+    <div className="App container-fluid">
       <h1 className="my-4 text-center">Vibe Gallery</h1>
       <div className="row g-0">
         {imageList.map((filename, idx) => (
@@ -14,12 +25,42 @@ function App() {
                 src={imageBasePath + filename}
                 className="card-img-top rounded-0"
                 alt={filename}
-                style={{ objectFit: "cover", height: "100%" }}
+                style={{ objectFit: "cover", height: "100%", cursor: "pointer" }}
+                onClick={() => handleImgClick(filename)}
               />
             </div>
           </div>
         ))}
       </div>
+
+      {fullscreenImg && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            background: "rgba(0,0,0,0.9)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            zIndex: 9999,
+          }}
+          onClick={handleClose}
+        >
+          <img
+            src={imageBasePath + fullscreenImg}
+            alt={fullscreenImg}
+            style={{
+              maxWidth: "90vw",
+              maxHeight: "90vh",
+              boxShadow: "0 0 40px #000",
+              borderRadius: "8px",
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 }
